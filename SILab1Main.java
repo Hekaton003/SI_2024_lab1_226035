b92136f44b53c5f5dc80f1cd42ce7079c02fef5c
Jovan Davaliev <jovandavaliev@gmail.com>
2024-03-31 23:35:40 +0200

    Call method makeBigStringFrom in main class

--- SILab1Main.java
+++ SILab1Main.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 public class SILab1Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,5 +13,6 @@ public class SILab1Main {
             list.add(sc.next());
         }
         System.out.println(SILab1_1.filterOddLengthStrings(list));
+        System.out.println(SILab1_2.makeBigStringFrom(list)); 
     }
 }
